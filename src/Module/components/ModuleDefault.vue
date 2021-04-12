dbecc4422067285358b9b6c615486e79577e8c2f
juxsta <ericjrreyes2@gmail.com>
2021-04-12 01:34:39 -0700

    removed type

--- src/Module/components/ModuleDefault.vue
+++ src/Module/components/ModuleDefault.vue
@@ -744,8 +744,7 @@ export default defineComponent({
       type: Object as () => User
     },
     getStudentDoc: {
-      required: true,
-      type: Object as () => any
+      required: true
     }
   },
 
